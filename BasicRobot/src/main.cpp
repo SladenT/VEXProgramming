2e61368c8a87c41b36740e100073f25155031b52
Davis Teigeler <davisteigeler@gmail.com>
2022-02-08 19:20:25 -0500

    oopsie

---
+++ BasicRobot/src/main.cpp
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\SURoC                                            */
+/*    Created:      Thu Jan 13 2022                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Motor15              motor         15              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+#include "DriverControls.h"
+#include "Kinetics.h"
+
+using namespace vex;
+
+bool exec;
+
+//Main Loop
+int main() {
+
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+
+
+  exec = true;
+  while (exec)
+  {
+    control::ControlBot();
+  }
+  
+
+}
